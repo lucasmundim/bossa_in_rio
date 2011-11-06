1128bcd2627414aab83730b7827fc7e27213025e
Lucas Roxo Mundim <lucas.mundim@gmail.com>
2011-11-06 06:03:47 -0200

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BossaInRio::Application.config.secret_token = '7200c0e3adac5cd040c369d7159f956b4a7f02fdbdd1b5603f30412e4880df740ba1d666d2188b2b8d883fe133d8dad6f7ab3e6713a8def7aac2c700d206f96d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BossaInRio::Application.config.session_store :cookie_store, key: '_bossa_in_rio_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BossaInRio::Application.config.session_store :active_record_store
