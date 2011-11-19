e349d9c3c938b4407128c514b313795dd6741020
Vicente Mundim <vicente.mundim@gmail.com>
2011-11-19 20:38:39 -0200

    Adding request specs to admin pages

--- spec/requests/admin/admin_pages_spec.rb
+++ spec/requests/admin/admin_pages_spec.rb
@@ -1,11 +1,49 @@
 require 'spec_helper'
 
 describe "Admin::Pages" do
-  describe "GET /admin/pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get admin_pages_path
-      response.status.should be(200)
+  let!(:home_page) do
+    Page.create :slug => :home, :snippets_attributes => [
+      {
+        :section => :bottom_left,
+        :translations => [
+          { :locale => 'pt-BR', :body => "Some contents here" }
+        ]
+      },
+      {
+        :section => :bottom_right,
+        :translations => [
+          { :locale => 'pt-BR', :body => "Some more contents here" }
+        ]
+      }
+    ]
+  end
+
+
+  let!(:photos_page) do
+    Page.create :slug => :photos
+  end
+
+  describe "when listing the pages" do
+    it "should list the existing pages" do
+      visit admin_pages_path
+      Page.all.each do |page_data|
+        page.should have_content(page_data.slug)
+      end
+    end
+
+    it "should display a link to the details of each of the existing pages" do
+      visit admin_pages_path
+      Page.all.each do |page_data|
+        page.should have_link(page_data.slug)
+      end
+    end
+  end
+
+  describe "when displaying page details" do
+    it "should go the the page details page after clicking on a page in the listing" do
+      visit admin_pages_path
+      click_link home_page.slug
+      current_path.should eq(admin_page_path(home_page))
     end
   end
 end
