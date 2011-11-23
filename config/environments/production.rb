3ff6b54720788f4fa96da8ab584367d3d78a3540
Lucas Roxo Mundim <lucas.mundim@gmail.com>
2011-11-23 04:06:48 -0200

    Precompiling admin assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ BossaInRio::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( admin_application.js, admin_application.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
