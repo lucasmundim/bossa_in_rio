b7cdf0185fe4cc46694fd0646add038a91c6ce60
Lucas Roxo Mundim <lucas.mundim@gmail.com>
2011-11-25 03:47:09 -0200

    Implementing snippets update action

--- app/controllers/admin/snippets_controller.rb
+++ app/controllers/admin/snippets_controller.rb
@@ -3,4 +3,15 @@ class Admin::SnippetsController < Admin::ApplicationController
     @page = Page.find(params[:page_id])
     @snippet = @page.snippets.find(params[:id])
   end
+
+  def update
+    @page = Page.find(params[:page_id])
+    @snippet = @page.snippets.find(params[:id])
+
+    if @snippet.set_translations(params[:translations])
+      redirect_to admin_page_path(@page), :notice => 'Snippet atualizado com sucesso.'
+    else
+      render :action => "edit"
+    end
+  end
 end
