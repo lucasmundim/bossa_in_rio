57643fa5f5cd26b7750b435b3617c630465ec748
Lucas Roxo Mundim <lucas.mundim@gmail.com>
2011-12-04 02:07:52 -0200

    Ensuring app domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :set_locale
+  before_filter :ensure_domain
+  
+  APP_DOMAIN = 'www.bossainrio.com'
 
   def set_locale
     if params[:i18n_locale].present?
@@ -16,5 +19,12 @@ class ApplicationController < ActionController::Base
   def default_url_options(options={})
     logger.debug "default_url_options is passed options: #{options.inspect}\n"
     { :i18n_locale => I18n.locale }
+  end
+
+  def ensure_domain
+    if request.env['HTTP_HOST'] != APP_DOMAIN
+      # HTTP 301 is a "permanent" redirect
+      redirect_to "http://#{APP_DOMAIN}", :status => 301
+    end
   end
 end
