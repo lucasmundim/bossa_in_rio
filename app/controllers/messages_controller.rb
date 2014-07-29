39665ad394c1113291626dd0872ffdcfbf8ad81a
Vicente Mundim <vicente.mundim@gmail.com>
2014-07-29 12:16:28 -0300

    Ignorando nomes assim como emails

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,29 @@
 class MessagesController < ApplicationController
   def create
-    if ignored_emails.blank? || !ignored_emails.include?(params[:message][:email])
+    if !ignored?(params[:message])
       @message = Message.create(params[:message])
     end
   end
 
   private
 
+  def ignored?(message)
+    ignored_email?(message[:email]) || ignored_name?(message[:first_name], message[:last_name])
+  end
+
+  def ignored_email?(email)
+    ignored_emails.present? && ignored_emails.include?(email)
+  end
+
+  def ignored_name?(first_name, last_name)
+    ignored_names.present? && ignored_names.include?("#{first_name.try(:downcase)}-#{last_name.try(:downcase)}")
+  end
+
   def ignored_emails
-    (ENV['IGNORED_EMAILS'].presence || '').split(',')
+    (ENV['IGNORED_EMAILS'].presence || '').split(',').map(&:downcase)
+  end
+
+  def ignored_names
+    (ENV['IGNORED_NAMES'].presence || '').split(',').map(&:downcase)
   end
 end
